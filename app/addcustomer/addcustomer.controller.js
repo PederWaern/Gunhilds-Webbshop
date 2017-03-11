3631fcc2f73b9d6d4fee8240398a08c158a503c7
Peder Waern <pederwaern@hotmail.com>
2017-03-11 20:53:57 +0100

    Can create new user w/o validation

--- app/addcustomer/addcustomer.controller.js
+++ app/addcustomer/addcustomer.controller.js
@@ -1,5 +1,25 @@
 angular.module("addcustomer")
-    .controller("addCustomerController", ["$scope", "$location", "addCustomerService", function ($scope, $location, $addCustomerService ) {
+    .controller("addCustomerController", ["$scope", "$location", "addCustomerService", function ($scope, $location, addCustomerService ) {
+
+        $scope.createCustomer = function() {
+            var cust = {};
+            cust.firstName = $scope.firstName;
+            cust.lastName = $scope.lastName;
+            cust.email = $scope.email;
+            cust.phone = $scope.phone;
+            cust.password = $scope.password;
+            cust.address = $scope.address;
+            cust.postalCode = $scope.postalCode;
+            cust.city = $scope.city;
+
+            addCustomerService.createCustomer(cust).then(function successCallback(response) {
+                console.log("User was created successfully!")
+                
+            }, function errorCallback(response) {
+                console.log("Failed to create user");
+            });
+
+        }
 
         
         
--- app/addcustomer/addcustomer.service.js
+++ app/addcustomer/addcustomer.service.js
@@ -1,7 +1,9 @@
 angular.module("addcustomer")
 .factory("addCustomerService",["$http", function($http) {
     return { 
-        
+        createCustomer: function (customerDetails){
+            return $http.post("http://nackbutik.azurewebsites.net/api/customer", customerDetails);
+        },
     
      }
     
